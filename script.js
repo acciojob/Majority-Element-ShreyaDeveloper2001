73235632deb69f569e37e3e413ff3f5472ee52d5
ShreyaDeveloper2001 <134867803+ShreyaDeveloper2001@users.noreply.github.com>
2023-10-06 21:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+function majorityElement(nums) {
+  let candidate = null;
+  let count = 0;
+
+  for (const num of nums) {
+    if (count === 0) {
+      candidate = num;
+      count = 1;
+    } else if (num === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
+}
+
+// Example usage:
+const input = [2, 1, 2];
+const result = majorityElement(input);
+console.log(result); // Output: 2
+
